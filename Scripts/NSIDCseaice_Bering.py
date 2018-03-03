56310312dcdc31852ca9adbc66a019aca28be049
Zachary Labe <zlabe@uci.edu>
2018-03-02 16:25:17 -0800

    add figs

---
+++ Scripts/NSIDCseaice_Bering.py
@@ -0,0 +1,132 @@
+"""
+Reads in current year's Bering Arctic sea ice extent from 
+Sea Ice Index 3 (NSIDC)
+
+Website   : ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/seaice_analysis/
+Author    : Zachary M. Labe
+Date      : 2 March 2018
+"""
+
+### Import modules
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+import cmocean
+
+### Directory and time
+directoryfigure = '/home/zlabe/Documents/Projects/BeringSeaIce2018/Figures/'
+now = datetime.datetime.now()
+currentmn = str(now.month)
+currentdy = str(now.day)
+currentyr = str(now.year)
+currenttime = currentmn + '_' + currentdy + '_' + currentyr
+currentdoy = now.timetuple().tm_yday
+doy = np.arange(0,365,1)
+lastday = now.timetuple().tm_yday -2
+years = np.arange(1979,2018+1,1)
+
+### Load url
+url = 'ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/seaice_analysis/' \
+        'Sea_Ice_Index_Regional_Daily_Data_G02135_v3.0.xlsx'
+
+## Read file
+df_bering = pd.read_excel(url,sheetname='Bering-Extent-km^2',header=1,
+                            parse_cols=range(3,43,1))
+bering = df_bering.as_matrix()
+
+sie = bering/1e6
+print('\nCompleted: Read sea ice data!')           
+
+### Missing data in 2017
+ice17q = sie[:,-2]
+ice17q[np.where(np.isnan(ice17q))] = 5.66287231e-01
+
+### Create running mean
+N = 14
+
+meanq = np.nanmean(sie[:,:],axis=1)
+mean = np.convolve(np.append(meanq[-14:],meanq), np.ones((N,))/N, mode='valid') 
+ice17 = np.convolve(np.append(sie[-14:,-3],ice17q),np.ones((N,))/N,mode='valid') 
+ice18 = np.convolve(np.append(sie[-14:,-2],sie[:,-1]),np.ones((N,))/N,mode='valid')            
+
+###########################################################################
+###########################################################################
+###########################################################################
+### Create plot
+plt.rc('text',usetex=True)
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Avant Garde']}) 
+plt.rc('savefig',facecolor='black')
+plt.rc('axes',edgecolor='darkgrey')
+plt.rc('xtick',color='white')
+plt.rc('ytick',color='white')
+plt.rc('axes',labelcolor='darkgrey')
+plt.rc('axes',facecolor='black')
+
+### Adjust axes in time series plots 
+def adjust_spines(ax, spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 5))
+        else:
+            spine.set_color('none')  
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+        ax.xaxis.set_ticks([]) 
+        
+xlabels = [r'Jan',r'Feb',r'Mar',r'Apr',r'May',r'Jun',r'Jul',
+          r'Aug',r'Sep',r'Oct',r'Nov',r'Dec',r'Jan']
+
+fig = plt.figure()
+ax = plt.subplot(111)
+
+ax.tick_params('both',length=5.5,width=2,which='major',color='darkgrey')             
+adjust_spines(ax, ['left','bottom'])            
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none') 
+ax.spines['bottom'].set_linewidth(2)
+ax.spines['left'].set_linewidth(2) 
+
+plt.axvline(30.4,color='darkgrey',linewidth=2,zorder=1,alpha=0.3)
+plt.axvline(60.8,color='darkgrey',linewidth=2,zorder=1,alpha=0.3)
+
+plt.plot(ice17,c='w',linewidth=1.3,zorder=3,alpha=1,label=r'\textbf{Year 2017}')
+plt.plot(ice18,c='r',linewidth=3,zorder=4,alpha=1,label=r'\textbf{Year 2018}')
+    
+plt.plot(mean,c='deepskyblue',linewidth=4,zorder=2,linestyle='--',
+         label=r'\textbf{1981-2010 Mean}',dashes=(1, 0.2))
+
+xlabels = [r'Jan',r'Feb',r'Mar',r'Apr',r'May',r'Jun',r'Jul',
+          r'Aug',r'Sep',r'Oct',r'Nov',r'Dec',r'Jan']
+plt.xticks(np.arange(0,361,30.4),xlabels,rotation=0,color='darkgrey')
+plt.yticks(np.arange(0,2.5,0.2),list(map(str,np.arange(0,2.5,0.2))),
+           color='darkgrey')
+plt.ylim([0,0.8])
+plt.xlim([0,361])
+
+fig.suptitle(r'\textbf{BERING SEA ICE}',
+                       fontsize=29,color='darkgrey')  
+
+plt.ylabel(r'\textbf{Extent [$\bf{\times 10^{6}}$\ \textbf{km}$\bf{^2}$]}',
+           fontsize=17,alpha=1,color='darkgrey',rotation=90) 
+
+le = plt.legend(shadow=False,fontsize=10,loc='upper center',
+           bbox_to_anchor=(0.51, -0.1),fancybox=True,frameon=False,ncol=3)
+for text in le.get_texts():
+    text.set_color('darkgrey') 
+    
+ax.annotate(r'\textbf{14-day running mean}',
+            xy=(0, 0),xytext=(0.81,0.87),xycoords='figure fraction',
+            fontsize=9,color='darkgrey',rotation=0,ha='center',va='center')
+    
+fig.subplots_adjust(bottom=0.15)
+       
+plt.savefig(directoryfigure + 'nsidc_sie_Bering.png',dpi=300)  
+
+print('Completed: Script done!')                          
--- Scripts/plot_BeringSeaIceConc.py
+++ Scripts/plot_BeringSeaIceConc.py
@@ -1,6 +1,5 @@
 """
-Plot reads in Bering Sea Ice Concentration data for February 1850-2018 and
-also creates a netcdf file of the data!
+Plots composites of Bering Sea Ice Conc compared to various baselines
 
 Notes
 -----
@@ -15,6 +14,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import cmocean
 import datetime
+import math
 
 ### Define directories
 directorydata = '/surtsey/zlabe/seaice_obs/SIC_Alaska/' 
@@ -33,27 +33,28 @@ print('\n' '----Plotting Bering SIC - %s----' % titletime)
 years = np.arange(1850,2017+1,1)
 
 ### Retrieve data from historical sea ice atlas
-filename = directorydata + 'SNAP_SEA_ICE_ATLAS.nc'
+filename = directorydata + 'Alaska_SIC_Feb_1850-2018.nc'
 
 data = Dataset(filename)
-ice = data.variables['sic_con_pct'][:]
+ice = data.variables['sic'][:]
 lat1 = data.variables['lat'][:]    
 lon1 = data.variables['lon'][:]
-time = data.variables['time'][:]
 data.close()
 
 print('Completed: Data read!')
 
-### Retrieve February data
-feb = ice[::12,:,:] # starts on 2/15/1850 to 2/15/2017
+### Mask values below 20%
+ice[np.where(ice<0.01)]=np.nan
 
 ### Meshgrid
 lon2,lat2 = np.meshgrid(lon1,lat1)
 
-### Read data for 2018
-filename2 = 'Alaska_SIC_Feb_2018.nc'
-data = Dataset(directorydata + filename2)
-ice18 = data.variables['sic'][:]
+### Baselines
+yearoldq = np.where((years>=1942) & (years<=1978))[0]
+yearnewq = np.where((years>=1979) & (years<=2017))[0]
+
+iceold = np.nanmean(ice[yearoldq,:,:],axis=0)
+icenew = np.nanmean(ice[yearnewq,:,:],axis=0)
 
 ###############################################################################
 ###############################################################################
@@ -68,44 +69,104 @@ plt.rc('ytick',color='white')
 plt.rc('axes',labelcolor='white')
 plt.rc('axes',facecolor='black')
 
-#def setcolor(x, color):
-#     for m in x:
-#         for t in x[m][1]:
-#             t.set_color(color)
-#
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-#m = Basemap(projection='npstere',boundinglat=57,lon_0=270,resolution='l',
-#            round =True,area_thresh=10000)
-#m.drawcoastlines(color = 'tomato',linewidth=0.4)
-#m.drawmapboundary(color='k')
-##m.drawlsmask(land_color='k',ocean_color='k')
-#
-##parallels = np.arange(50,86,5)
-##meridians = np.arange(-180,180,30)
-##m.drawparallels(parallels,labels=[False,False,False,False],linewidth=0.0,color='w')
-##par=m.drawmeridians(meridians,labels=[True,True,False,False],linewidth=0.0,fontsize=6,color='w')
-##setcolor(par,'white')
-#
-#cs = m.contourf(lon,lat,ice[:,:]*100.,np.arange(20,101,2),extend='min',latlon=True)
-#   
-#cmap = cmocean.cm.ice     
-#cs.set_cmap(cmap)
-#
-#m.fillcontinents(color='k')
-#
-#cbar = m.colorbar(cs,location='right',pad = 0.55)
-#ticks = np.arange(20,101,10)
-#labels = map(str,np.arange(20,101,10))
-#cbar.set_ticklabels(ticks,labels)
-#cbar.set_label(r'\textbf{ CONCENTRATION [\%]}',fontsize=13,color='darkgrey')
-#cbar.ax.tick_params(axis='y', size=.001)
-#                         
-#            
-#fig.subplots_adjust(top=0.905)
-#
-#print('Completed: Figure plotted!')
-#
-#plt.savefig(directorys + 'seaiceconc.png', dpi=300)
-#
-#print('Completed: Script done!')
+def setcolor(x, color):
+     for m in x:
+         for t in x[m][1]:
+             t.set_color(color)
+             
+### Alaska Region
+latmin = 50
+latmax = 77
+lonmin = 185
+lonmax = 220
+
+fig = plt.figure()
+
+def polar_stere(lon_w, lon_e, lat_s, lat_n, **kwargs):
+    '''Returns a Basemap object (NPS/SPS) focused in a region.
+    
+    lon_w, lon_e, lat_s, lat_n -- Graphic limits in geographical coordinates.
+                                  W and S directions are negative.
+    **kwargs -- Aditional arguments for Basemap object.
+    
+    '''
+    lon_0 = lon_w + (lon_e - lon_w) / 2.
+    ref = lat_s if abs(lat_s) > abs(lat_n) else lat_n
+    lat_0 = math.copysign(90., ref)
+    proj = 'npstere' if lat_0 > 0 else 'spstere'
+    prj = Basemap(projection=proj, lon_0=lon_0, lat_0=lat_0,
+                          boundinglat=0, resolution='l')
+    #prj = pyproj.Proj(proj='stere', lon_0=lon_0, lat_0=lat_0)
+    lons = [lon_w, lon_e, lon_w, lon_e, lon_0, lon_0]
+    lats = [lat_s, lat_s, lat_n, lat_n, lat_s, lat_n]
+    x, y = prj(lons, lats)
+    ll_lon, ll_lat = prj(min(x), min(y), inverse=True)
+    ur_lon, ur_lat = prj(max(x), max(y), inverse=True)
+    return Basemap(projection='stere', lat_0=lat_0, lon_0=lon_0,
+                       llcrnrlon=ll_lon, llcrnrlat=ll_lat,
+                       urcrnrlon=ur_lon, urcrnrlat=ur_lat, round=True,
+                       resolution='l')
+
+ax = fig.add_subplot(121)
+m = polar_stere(lonmin,lonmax,latmin,latmax)
+m.drawcoastlines(color = 'dimgrey',linewidth=0.8)
+m.drawmapboundary(color='k')
+m.drawlsmask(land_color='k',ocean_color='k')
+
+cs = m.contourf(lon2,lat2,iceold,np.arange(0,101,2),latlon=True)
+#cs1 = m.contour(lon2,lat2,iceold,np.arange(15,20,5),latlon=True,colors='m',
+#               linewidths=1)
+cs2 = m.contour(lon2,lat2,ice[-1,:,:],np.arange(15,20,5),latlon=True,colors='r',
+                linewidths=2)
+
+cmap = cmocean.cm.dense_r     
+cs.set_cmap(cmap)
+
+m.fillcontinents(color='k')
+
+ax.annotate(r'\textbf{1942--1978}',
+            xy=(0, 0),xytext=(0.5,1.02),xycoords='axes fraction',
+            fontsize=25,color='darkgrey',rotation=0,ha='center',va='center')
+
+###############################################################################
+ax1 = fig.add_subplot(122)
+m = polar_stere(lonmin,lonmax,latmin,latmax)
+m.drawcoastlines(color ='dimgrey',linewidth=0.8)
+m.drawmapboundary(color='k')
+m.drawlsmask(land_color='k',ocean_color='k')
+
+cs = m.contourf(lon2,lat2,icenew,np.arange(0,101,2),latlon=True)
+#cs1 = m.contour(lon2,lat2,icenew,np.arange(15,20,5),latlon=True,colors='m',
+#               linewidths=1)
+cs2 = m.contour(lon2,lat2,ice[-1,:,:],np.arange(15,20,5),latlon=True,colors='r',
+                linewidths=2)
+
+cmap = cmocean.cm.dense_r     
+cs.set_cmap(cmap)
+
+m.fillcontinents(color='k')
+
+cbar_ax = fig.add_axes([0.312,0.17,0.4,0.03])                
+cbar = fig.colorbar(cs,cax=cbar_ax,orientation='horizontal',
+                    extend='min',extendfrac=0.07,drawedges=False)
+barlim = np.arange(0,101,50)
+cbar.set_ticks(barlim)
+cbar.set_ticklabels(list(map(str,barlim))) 
+cbar.set_label(r'\textbf{SEA ICE CONCENTRATION [\%]}',fontsize=13,
+                         color='darkgrey',labelpad=-36)
+cbar.ax.tick_params(axis='x', size=.001)
+
+ax1.annotate(r'\textbf{1979--2017}',
+            xy=(0, 0),xytext=(0.5,1.02),xycoords='axes fraction',
+            fontsize=25,color='darkgrey',rotation=0,ha='center',va='center')
+ax1.annotate(r'\textbf{2018 -- Red}',
+            xy=(0, 0),xytext=(0.51,0.75),xycoords='figure fraction',
+            fontsize=9,color='r',rotation=0,ha='center',va='center')
+          
+fig.subplots_adjust(hspace=-0.6)
+
+print('Completed: Figure plotted!')
+
+plt.savefig(directoryfigure + 'baselines_Bering.png', dpi=300)
+
+print('Completed: Script done!')
